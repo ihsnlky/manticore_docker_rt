44de61858d38240317b46cbd8e7611f357cc4df1
Ilkay Cetindag <110613255+ihsnlky@users.noreply.github.com>
2023-08-29 21:46:34 +0300

    Delete clickhouse-client.sh

--- clickhouse-client.sh
+++
@@ -1,9 +0,0 @@
-apt-get install -y apt-transport-https ca-certificates dirmngr
-GNUPGHOME=$(mktemp -d)
-GNUPGHOME="$GNUPGHOME" gpg --no-default-keyring --keyring /usr/share/keyrings/clickhouse-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8919F6BD2B48D754
-rm -r "$GNUPGHOME"
-chmod +r /usr/share/keyrings/clickhouse-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/clickhouse-keyring.gpg] https://packages.clickhouse.com/deb stable main" | tee \
-    /etc/apt/sources.list.d/clickhouse.list
-apt-get update
-apt-get install clickhouse-client -y
