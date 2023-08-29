00a122c1842b6a37aa95814d66e453c8f8072834
Ilkay Cetindag <110613255+ihsnlky@users.noreply.github.com>
2023-08-29 23:29:54 +0300

    Update real_time.py

--- real_time.py
+++ real_time.py
@@ -5,12 +5,12 @@ from pymysql import connect
 clickhouse_client = Client(host='clickhouse', port=9000, user='default', password='default', database='default')
 manticore_connection = connect(host='localhost', port=9306) 
 manticore_cursor = manticore_connection.cursor()
-index = 'explanation_idx'
+index = 'pickup_ntaname_idx'
 while True:
-    query = 'SELECT CRC64(explanation), explanation FROM trips'
+    query = 'SELECT CRC64(pickup_ntaname), pickup_ntaname FROM trips'
     result = clickhouse_client.execute(query)
     for row in result:
-        insert_query = f"INSERT INTO {index} (id, explanation) VALUES ({row[0]}, '{row[1]}')"
+        insert_query = f"INSERT INTO {index} (id, pickup_ntaname) VALUES ({row[0]}, '{row[1]}')"
         manticore_cursor.execute(insert_query)
     manticore_connection.commit()
     time.sleep(2000)
