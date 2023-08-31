93e1f96f8c2279afcde86fa4fd15962d5892e46a
Ilkay Cetindag <110613255+ihsnlky@users.noreply.github.com>
2023-08-31 12:11:53 +0300

    Update real_time.py

--- real_time.py
+++ real_time.py
@@ -2,14 +2,14 @@ import time
 from clickhouse_driver import Client
 from pymysql import connect
 
-clickhouse_client = Client(host='***', port=***, user='***', password='***', secure=True)
+clickhouse_client = Client(host='***', port=***, user='***', password='***', secure=True) #Replace the ChistaDATA DBaaS environment informations please.
 manticore_connection = connect(host='localhost', port=9306) 
 manticore_cursor = manticore_connection.cursor()
-index = 'explanation_idx'
+index = 'string_column_name_idx' #Replace the string_column_name to your String column please. 
 while True:
-    query = 'SELECT CRC64(explanation), explanation FROM trips'
+    query = 'SELECT CRC64(string_column_name), string_column_name FROM trips'
     result = clickhouse_client.execute(query)
     for row in result:
-        insert_query = f"INSERT INTO {index} (id, explanation) VALUES ({row[0]}, '{row[1]}')"
+        insert_query = f"INSERT INTO {index} (id, string_column_name) VALUES ({row[0]}, '{row[1]}')"
         manticore_cursor.execute(insert_query)
     manticore_connection.commit()
